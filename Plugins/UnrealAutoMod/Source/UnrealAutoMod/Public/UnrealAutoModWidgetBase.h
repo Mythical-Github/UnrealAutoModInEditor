42eb98e66e246194b3eb828fac768adfac71a373
Mythical <MythicalData@gmail.com>
2024-06-20 19:44:45 -0400

    testing

---
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModWidgetBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UnrealAutoModWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALAUTOMOD_API UUnrealAutoModWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
