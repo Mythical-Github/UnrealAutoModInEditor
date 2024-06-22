5132acb8e5bd5fdb313112cc0510804dd0723d95
Mythical <MythicalData@gmail.com>
2024-06-21 23:24:58 -0400

    Added valid class and object functions

--- Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.cpp
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.cpp
@@ -2,20 +2,16 @@
 #include "UnrealAutoModBaseWidget.h"
 #include "Runtime/Core/Public/HAL/PlatformProcess.h" // Include for launching processes
 #include "Runtime/Core/Public/Misc/Paths.h" // Include for file path utilities
-#include "UObject.h" // Include for UObject
 
 void UUnrealAutoModBaseWidget::LaunchExternalExecutable(const FString& FilePath, const TArray<FString>& Parameters)
 {
-    // Construct the command string
     FString Command = FString::Printf(TEXT("\"%s\""), *FilePath);
 
-    // Append each parameter to the command string
     for (const FString& Param : Parameters)
     {
         Command += FString::Printf(TEXT(" %s"), *Param);
     }
 
-    // Launch the process
     FProcHandle ProcessHandle = FPlatformProcess::CreateProc(*FilePath, *Command, true, false, false, nullptr, 0, nullptr, nullptr);
 
     if (!ProcessHandle.IsValid())
@@ -26,15 +22,23 @@ void UUnrealAutoModBaseWidget::LaunchExternalExecutable(const FString& FilePath,
 
 FString UUnrealAutoModBaseWidget::GetProjectDirectory()
 {
-    // Get the directory of the .uproject file
     FString ProjectFilePath = FPaths::ConvertRelativePathToFull(FPaths::GetProjectFilePath());
-    FString ProjectDir = FPaths::GetPath(ProjectFilePath); // Extract the directory part
+    FString ProjectDir = FPaths::GetPath(ProjectFilePath);
 
     return ProjectDir;
 }
 
 UObject* UUnrealAutoModBaseWidget::LoadObjectByPath(const FString& ObjectPath)
 {
-    // Attempt to load the object from the given path
     return StaticLoadObject(UObject::StaticClass(), nullptr, *ObjectPath, nullptr, RF_NoFlags, nullptr);
+}
+
+bool UUnrealAutoModBaseWidget::IsValidObject(UObject* Object)
+{
+    return IsValid(Object);
+}
+
+bool UUnrealAutoModBaseWidget::IsValidClass(UClass* Class)
+{
+    return (Class != nullptr && Class->IsValidLowLevel());
 }
--- Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.h
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.h
@@ -2,8 +2,8 @@
 
 #include "UnrealAutoModPrivatePCH.h"
 #include "Blueprint/UserWidget.h"
-#include "Runtime/Core/Public/HAL/PlatformProcess.h" // Include for launching processes
-#include "Runtime/Core/Public/Misc/Paths.h" // Include for file path utilities
+#include "UObject/UObject.h" // Include for UObject
+#include "UObject/Class.h"   // Include for UClass
 
 #include "UnrealAutoModBaseWidget.generated.h"
 
@@ -16,15 +16,18 @@ class UNREALAUTOMOD_API UUnrealAutoModBaseWidget : public UUserWidget
     GENERATED_BODY()
 
 public:
-    // Function declaration to launch an external executable with an array of parameters
     UFUNCTION(BlueprintCallable, Category = "UnrealAutoMod")
         void LaunchExternalExecutable(const FString& FilePath, const TArray<FString>& Parameters);
 
-    // Function declaration to get the directory of the .uproject file
     UFUNCTION(BlueprintCallable, Category = "UnrealAutoMod")
         FString GetProjectDirectory();
 
-    // Function declaration to load an object based on its path
     UFUNCTION(BlueprintCallable, Category = "UnrealAutoMod")
         UObject* LoadObjectByPath(const FString& ObjectPath);
+
+    UFUNCTION(BlueprintCallable, Category = "UnrealAutoMod")
+        bool IsValidObject(UObject* Object);
+
+    UFUNCTION(BlueprintCallable, Category = "UnrealAutoMod")
+        bool IsValidClass(UClass* Class);
 };
