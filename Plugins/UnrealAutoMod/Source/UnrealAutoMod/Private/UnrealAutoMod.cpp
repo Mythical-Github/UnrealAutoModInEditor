4d0e5762fa95af048378770ade7cd2087bf8231f
Mythical <MythicalData@gmail.com>
2024-06-22 02:43:29 -0400

    Added the WBP_UnrealAutoMod into the Slate Panel

--- Plugins/UnrealAutoMod/Source/UnrealAutoMod/Private/UnrealAutoMod.cpp
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod/Private/UnrealAutoMod.cpp
@@ -10,6 +10,18 @@
 
 #include "LevelEditor.h"
 
+#include "UnrealAutoMod.h" // Your module's header file
+#include "SlateBasics.h"
+#include "SlateExtras.h"
+#include "Widgets/Docking/SDockTab.h"
+#include "Widgets/Layout/SBox.h"
+#include "Widgets/Text/STextBlock.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "Engine/World.h"
+#include "Editor/EditorEngine.h"
+#include "Editor.h"
+
 static const FName UnrealAutoModTabName("UnrealAutoMod");
 
 #define LOCTEXT_NAMESPACE "FUnrealAutoModModule"
@@ -64,25 +76,67 @@ void FUnrealAutoModModule::ShutdownModule()
 
 TSharedRef<SDockTab> FUnrealAutoModModule::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
 {
-	FText WidgetText = FText::Format(
-		LOCTEXT("WindowWidgetText", "Add code to {0} in {1} to override this window's contents"),
-		FText::FromString(TEXT("FUnrealAutoModModule::OnSpawnPluginTab")),
-		FText::FromString(TEXT("UnrealAutoMod.cpp"))
-		);
+    // Load the UserWidget class from the Blueprint
+    UClass* UserWidgetClass = LoadObject<UClass>(nullptr, TEXT("/UnrealAutoMod/WBP_UnrealAutoMod.WBP_UnrealAutoMod_C"));
 
-	return SNew(SDockTab)
-		.TabRole(ETabRole::NomadTab)
-		[
-			// Put your tab content here!
-			SNew(SBox)
-			.HAlign(HAlign_Center)
-			.VAlign(VAlign_Center)
-			[
-				SNew(STextBlock)
-				.Text(WidgetText)
-			]
-		];
+    if (!UserWidgetClass)
+    {
+        // Handle the error: UserWidget class not found
+        FText ErrorText = FText::Format(
+            LOCTEXT("ErrorLoadingWidget", "Failed to load UserWidget class from {0}"),
+            FText::FromString(TEXT("/UnrealAutoMod/WBP_UnrealAutoMod.WBP_UnrealAutoMod_C"))
+        );
+
+        return SNew(SDockTab)
+            .TabRole(ETabRole::NomadTab)
+            [
+                SNew(SBox)
+                .HAlign(HAlign_Center)
+            .VAlign(VAlign_Center)
+            [
+                SNew(STextBlock)
+                .Text(ErrorText)
+            ]
+            ];
+    }
+
+    // Create an instance of the UserWidget
+    UUserWidget* UserWidget = CreateWidget<UUserWidget>(GEditor->GetEditorWorldContext().World(), UserWidgetClass);
+
+    if (!UserWidget)
+    {
+        // Handle the error: UserWidget instance could not be created
+        FText ErrorText = FText::Format(
+            LOCTEXT("ErrorCreatingWidget", "Failed to create UserWidget instance from {0}"),
+            FText::FromString(TEXT("/UnrealAutoMod/WBP_UnrealAutoMod.WBP_UnrealAutoMod_C"))
+        );
+
+        return SNew(SDockTab)
+            .TabRole(ETabRole::NomadTab)
+            [
+                SNew(SBox)
+                .HAlign(HAlign_Center)
+            .VAlign(VAlign_Center)
+            [
+                SNew(STextBlock)
+                .Text(ErrorText)
+            ]
+            ];
+    }
+
+    // Add the UserWidget to the tab
+    return SNew(SDockTab)
+        .TabRole(ETabRole::NomadTab)
+        [
+            SNew(SBox)
+            .HAlign(HAlign_Fill)
+        .VAlign(VAlign_Fill)
+        [
+            UserWidget->TakeWidget()
+        ]
+        ];
 }
+
 
 void FUnrealAutoModModule::PluginButtonClicked()
 {
