378e604821a923b478eaef1eb17e53da5b34ecee
Mythical <MythicalData@gmail.com>
2024-06-30 01:34:43 -0400

    wip datatable functions

--- Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.cpp
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.cpp
@@ -1,2 +1,44 @@
 #include "UnrealAutoModPrivatePCH.h"
 #include "UnrealAutoModBaseWidget.h"
+
+FString UUnrealAutoModBaseWidget::DataTableToJson(UDataTable* DataTable)
+{
+    if (!DataTable)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("DataTable is null"));
+        return "";
+    }
+
+    FString OutputString;
+    TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+
+    if (DataTable->WriteTableAsJSON(Writer))
+    {
+        Writer->Close();
+        return OutputString;
+    }
+
+    return "";
+}
+
+bool UUnrealAutoModBaseWidget::JsonToDataTable(const FString& JsonString, UDataTable* DataTable)
+{
+    if (!DataTable)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("DataTable is null"));
+        return false;
+    }
+
+    TArray<FString> Problems = DataTable->CreateTableFromJSONString(JsonString);
+    if (Problems.Num() > 0)
+    {
+        for (const FString& Problem : Problems)
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Problem: %s"), *Problem);
+        }
+        return false;
+    }
+
+    return true;
+}
+
--- Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.h
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod/Public/UnrealAutoModBaseWidget.h
@@ -1,11 +1,21 @@
 #pragma once
 
 #include "UnrealAutoModPrivatePCH.h"
-#include "UserWidget.h"
+#include "Blueprint/UserWidget.h"
+#include "Json.h"
+#include "Runtime/JsonUtilities/Public/JsonUtilities.h"
+#include "Engine/DataTable.h"
 #include "UnrealAutoModBaseWidget.generated.h"
 
 UCLASS()
 class UNREALAUTOMOD_API UUnrealAutoModBaseWidget : public UUserWidget
 {
     GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "JSON")
+        FString DataTableToJson(UDataTable* DataTable);
+
+    UFUNCTION(BlueprintCallable, Category = "JSON")
+        bool JsonToDataTable(const FString& JsonString, UDataTable* DataTable);
 };
