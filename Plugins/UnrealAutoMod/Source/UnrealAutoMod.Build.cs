fd729af20e1394ba64c5f8d2a78962883bfc806b
Mythical-Github <MythicalData@gmail.com>
2024-09-09 20:39:49 -0400

    Update UnrealAutoMod.Build.cs

--- Plugins/UnrealAutoMod/Source/UnrealAutoMod.Build.cs
+++ Plugins/UnrealAutoMod/Source/UnrealAutoMod.Build.cs
@@ -38,6 +38,8 @@ public class UnrealAutoMod : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+                "Json",
+                "JsonUtilities"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
